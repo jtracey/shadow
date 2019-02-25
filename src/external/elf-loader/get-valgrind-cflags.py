62ea291e91cc8b52c87949f141ba15db2e025ab6
Justin Tracey <j3tracey@uwaterloo.ca>
2019-02-25 10:41:21 -0500

    switch elf-loader build to python3

--- src/external/elf-loader/get-valgrind-cflags.py
+++ src/external/elf-loader/get-valgrind-cflags.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 
 if os.path.exists ('/usr/include/valgrind/valgrind.h'):
-    print '-DHAVE_VALGRIND_H -I/usr/include'
+    print('-DHAVE_VALGRIND_H -I/usr/include')
